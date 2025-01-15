b5bbdb530f114143439608b2369e905844355778
Shaun Levy <SLY@ndonline.ac.uk>
2025-01-15 14:32:57 +0000

    Set up some intial pages

--- TL_SLY_GJ/Program.cs
+++ TL_SLY_GJ/Program.cs
@@ -1,10 +1,13 @@
 using TL_SLY_GJ.Components;
+using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddMudServices();
 
 var app = builder.Build();
 
